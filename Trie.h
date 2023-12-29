6656b8ff07446e6f33094cbc7ebb1247e771ea54
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-29 00:57:21 +0000

    Create Trie.h

---
+++ Trie.h
@@ -0,0 +1,29 @@
+// Trie.h
+#pragma once
+
+#include <iostream>
+#include <unordered_map>
+
+class TrieNode 
+{
+public:
+    std::unordered_map<char, TrieNode*> children;
+    bool isEndOfWord;
+
+    TrieNode();
+};
+
+class Trie 
+{
+private:
+    TrieNode* root;
+
+public:
+    Trie();
+    ~Trie();
+
+    void Insert(const std::string& word);
+    bool Search(const std::string& word) const;
+    bool StartsWith(const std::string& prefix) const;
+    void DeleteTrie(TrieNode* node);
+};
